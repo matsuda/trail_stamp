b722edb559b694591ae76a49c4f7ae82e5df1ee8
Kosuke Matsuda <kosukematsuda@gmail.com>
2010-09-19 15:28:05 +0900

    インデントの整理 Rakeタスクディレクトリをlib配下へ移動

--- generators/trail_stamp_migration/templates/migration.rb
+++ generators/trail_stamp_migration/templates/migration.rb
@@ -1,18 +1,18 @@
 class TrailStampMigration < ActiveRecord::Migration
   def self.up
     create_table :monitor_trails do |t|
-      t.integer       :stamper_id,       :comment => "操作者ID"
-      t.string        :stamper_type,     :comment => "操作者モデル"
-      t.string        :controller,     :comment => "コントローラー"
-      t.string        :action,     :comment => "操作"
+      t.integer       :stamper_id,        :comment => "操作者ID"
+      t.string        :stamper_type,      :comment => "操作者モデル"
+      t.string        :controller,        :comment => "コントローラー"
+      t.string        :action,            :comment => "操作"
       t.timestamps
     end
     add_index :monitor_trails, [:stamper_id, :stamper_type]
 
     create_table :monitor_stamps do |t|
-      t.integer       :trail_id,       :comment => "証跡ID"
-      t.integer       :stampable_id,       :comment => "操作対象ID"
-      t.string        :stampable_type,     :comment => "操作対象モデル"
+      t.integer       :trail_id,          :comment => "証跡ID"
+      t.integer       :stampable_id,      :comment => "操作対象ID"
+      t.string        :stampable_type,    :comment => "操作対象モデル"
       t.timestamps
     end
     add_index :monitor_stamps, :trail_id
